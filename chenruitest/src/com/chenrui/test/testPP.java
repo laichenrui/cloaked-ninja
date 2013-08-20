fa1fe5533442f8919dc4b419bd20cba75d2c91e2
laichenrui <laichenrui@gmail.com>
2013-08-20 22:17:13 +0800

    first commit

---
+++ chenruitest/src/com/chenrui/test/testPP.java
@@ -0,0 +1,21 @@
+package com.chenrui.test;
+
+public class testPP {
+	public static void main(String args[]) {
+		int i, j;
+
+		i = 0;
+
+		for (j = 0; j < 5; j++)
+
+		{
+
+			i = i++;
+
+			System.out.println("i=" + i);
+
+		}
+
+	}
+
+}
